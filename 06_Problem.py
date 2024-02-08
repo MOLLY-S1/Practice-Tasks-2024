582a0346e58d5c5df0b8c7f5b59d3bc4a088cc29
MOLLY-S1 <sankeym@middleton.school.nz>
2024-02-08 21:11:41 +1300

    Sandwich

--- 06_Problem.py
+++ 06_Problem.py
@@ -1,3 +1,4 @@
 numbers = [20, 36, 12, 24, 20, 48, 74, 353, 23, 98]
 biggest = max(numbers)
 print(biggest)
+
--- 07_Problem.py
+++ 07_Problem.py
@@ -10,42 +10,124 @@ meats = {"Chicken": 2.69,
          "Salami": 4,
          "Vegan Slice": 3.3}
 
-garnishs = {"Onion": 1.69,
-            "Tomato": 1,
-            "Lettuce": 2,
-            "Cheese": 2.5}
+garnishes = {"Onion": 1.69,
+             "Tomato": 1,
+             "Lettuce": 2,
+             "Cheese": 2.5}
 
+price_add = 0
 
-def garnish():
-    garnish_choice = easygui.choicebox("Please Choose your garnish:",
-                                       "Garnish",
-                                       choices=["Onion: 1.69",
-                                                "Tomato: 1",
-                                                "Lettuce: 2",
-                                                "Cheese: 2.5"])
+bread_choice = easygui.buttonbox("Please Choose your Bread:\n"
+                                 "Wholemeal: $1\n"
+                                 "White: $0.8\n"
+                                 "Cheesy White: $1.2\n"
+                                 "Gluten Free: $1.4\n",
+                                 "BREAD",
+                                 choices=["Wholemeal", "White",
+                                          "Cheesy White", "Gluten Free"])
+price_add += breads[bread_choice]
 
-    return garnish_choice
+meat_choice = easygui.buttonbox("Please Choose your meat:\n"
+                                "Chicken: $2.69\n" "Beef: $3\n"
+                                "Salami: $4\n" "Vegan Slice: $3.3\n",
+                                "MEAT",
+                                choices=["Chicken", "Beef",
+                                         "Salami", "Vegan Slice"])
+price_add += meats[meat_choice]
 
+garnish_choice = easygui.buttonbox("Please Choose your garnish:\n"
+                                   "Onion: $1.69\n"
+                                   "Tomato: $1\n"
+                                   "Lettuce: $2\n"
+                                   "Cheese: $2.5\n",
+                                   "GARNISH",
+                                   choices=["Onion",
+                                            "Tomato",
+                                            "Lettuce",
+                                            "Cheese"])
 
-def meat():
-    meat_choice = easygui.choicebox("Please Choose your meat:", "Meat",
-                                    choices=["Chicken: 2.69", "Beef: 3",
-                                             "Salami: 4", "Vegan Slice: 3.3"])
+price_add += garnishes[garnish_choice]
 
-    return meat_choice
+confirm = easygui.buttonbox(f"Is the following combo correct?\n"
+                            f"--------------------------------\n"
+                            f"Bread: {bread_choice} - ${breads[bread_choice]}\n"
+                            f"Meat: {meat_choice} - ${meats[meat_choice]}\n"
+                            f"Garnish: {garnish_choice} - "
+                            f"${garnishes[garnish_choice]}\n"
+                            f"Total: ${price_add:.2f}", "CONFIRM", choices=["Yes",
+                                                                        "No"])
 
+if confirm == "Yes":
+    easygui.msgbox("Thanks for ordering!", "GOODBYE")
 
-def bread():
-    bread_choice = easygui.choicebox("Please Choose your bread:", "Bread",
-                                     choices=["Wholemeal: 1",
-                                              "White: 0.8",
-                                              "Cheesy White: 1.2",
-                                              "Gluten Free: 1.4"])
-    return bread_choice
+while confirm == "No":
+    change = easygui.buttonbox(f"What would you like to change:\n", "CHANGE "
+                                                                    "SANDWICH",
+                               choices=["Bread", "Meat", "Garnish"])
+    if change == "Bread":
+        price_add -= breads[bread_choice]
+        bread_choice = easygui.buttonbox("Please Choose your Bread:\n"
+                                         "Wholemeal: $1\n"
+                                         "White: $0.8\n"
+                                         "Cheesy White: $1.2\n"
+                                         "Gluten Free: $1.4\n",
+                                         "BREAD",
+                                         choices=["Wholemeal", "White",
+                                                  "Cheesy White",
+                                                  "Gluten Free"])
+        price_add += breads[bread_choice]
+        confirm = easygui.buttonbox(f"Is the following combo correct?\n"
+                                    f"--------------------------------\n"
+                                    f"Bread: {bread_choice} - ${breads[bread_choice]}\n"
+                                    f"Meat: {meat_choice} - ${meats[meat_choice]}\n"
+                                    f"Garnish: {garnish_choice} - "
+                                    f"${garnishes[garnish_choice]}\n"
+                                    f"Total: ${price_add:.2f}", "CONFIRM",
+                                    choices=["Yes",
+                                             "No"])
 
+    if change == "Meat":
+        price_add -= meats[meat_choice]
+        meat_choice = easygui.buttonbox("Please Choose your meat:\n"
+                                        "Chicken: $2.69\n" "Beef: $3\n"
+                                        "Salami: $4\n" "Vegan Slice: $3.3\n",
+                                        "MEAT",
+                                        choices=["Chicken", "Beef",
+                                                 "Salami", "Vegan Slice"])
+        price_add += meats[meat_choice]
+        confirm = easygui.buttonbox(f"Is the following combo correct?\n"
+                                    f"--------------------------------\n"
+                                    f"Bread: {bread_choice} - ${breads[bread_choice]}\n"
+                                    f"Meat: {meat_choice} - ${meats[meat_choice]}\n"
+                                    f"Garnish: {garnish_choice} - "
+                                    f"${garnishes[garnish_choice]}\n"
+                                    f"Total: ${price_add:.2f}", "CONFIRM",
+                                    choices=["Yes",
+                                             "No"])
 
-# main routine
-bread()
-meat()
-garnish()
-print(f"your order is {bread()} with {meat()} and {garnish()}")
+    if change == "Garnish":
+        price_add -= garnishes[garnish_choice]
+        garnish_choice = easygui.buttonbox("Please Choose your garnish:\n"
+                                           "Onion: $1.69\n"
+                                           "Tomato: $1\n"
+                                           "Lettuce: $2\n"
+                                           "Cheese: $2.5\n",
+                                           "GARNISH",
+                                           choices=["Onion",
+                                                    "Tomato",
+                                                    "Lettuce",
+                                                    "Cheese"])
+
+        price_add += garnishes[garnish_choice]
+        confirm = easygui.buttonbox(f"Is the following combo correct?\n"
+                                    f"--------------------------------\n"
+                                    f"Bread: {bread_choice} - ${breads[bread_choice]}\n"
+                                    f"Meat: {meat_choice} - ${meats[meat_choice]}\n"
+                                    f"Garnish: {garnish_choice} - "
+                                    f"${garnishes[garnish_choice]}\n"
+                                    f"Total: ${price_add:.2f}", "CONFIRM",
+                                    choices=["Yes",
+                                             "No"])
+
+if confirm == "Yes":
+    easygui.msgbox("Thanks for ordering!", "GOODBYE")
