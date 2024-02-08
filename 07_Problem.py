17b1ee84186897798424e46d73088c3921071514
MOLLY-S1 <sankeym@middleton.school.nz>
2024-02-08 14:17:05 +1300

    Sandwiches

--- 07_Problem.py
+++ 07_Problem.py
@@ -1,16 +1,51 @@
-import easy
-bread = {"Wholemeal": 1,
-         "White": 0.8,
-         "Cheesy White": 1.2,
-         "Gluten Free": 1.4}
+import easygui
 
-meat = {"Chicken": 2.69,
-        "Beef": 3,
-        "Salami": 4,
-        "Vegan Slice": 3.3}
+breads = {"Wholemeal": 1,
+          "White": 0.8,
+          "Cheesy White": 1.2,
+          "Gluten Free": 1.4}
 
-garnish = {"Onion": 1.69,
-           "Tomato": 1,
-           "Lettuce": 2,
-           "Cheese": 2.5}
+meats = {"Chicken": 2.69,
+         "Beef": 3,
+         "Salami": 4,
+         "Vegan Slice": 3.3}
 
+garnishs = {"Onion": 1.69,
+            "Tomato": 1,
+            "Lettuce": 2,
+            "Cheese": 2.5}
+
+
+def garnish():
+    garnish_choice = easygui.choicebox("Please Choose your garnish:",
+                                       "Garnish",
+                                       choices=["Onion: 1.69",
+                                                "Tomato: 1",
+                                                "Lettuce: 2",
+                                                "Cheese: 2.5"])
+
+    return garnish_choice
+
+
+def meat():
+    meat_choice = easygui.choicebox("Please Choose your meat:", "Meat",
+                                    choices=["Chicken: 2.69", "Beef: 3",
+                                             "Salami: 4", "Vegan Slice: 3.3"])
+
+    return meat_choice
+
+
+def bread():
+    bread_choice = easygui.choicebox("Please Choose your bread:", "Bread",
+                                     choices=["Wholemeal: 1",
+                                              "White: 0.8",
+                                              "Cheesy White: 1.2",
+                                              "Gluten Free: 1.4"])
+    return bread_choice
+
+
+# main routine
+bread()
+meat()
+garnish()
+print(f"your order is {bread()} with {meat()} and {garnish()}")
