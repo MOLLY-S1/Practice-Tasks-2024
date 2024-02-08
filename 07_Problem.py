14fafcce6b55302042ef1c90ae3af84651a0cf93
MOLLY-S1 <sankeym@middleton.school.nz>
2024-02-08 13:57:01 +1300

    Easy Gui

---
+++ 07_Problem.py
@@ -0,0 +1,16 @@
+import easy
+bread = {"Wholemeal": 1,
+         "White": 0.8,
+         "Cheesy White": 1.2,
+         "Gluten Free": 1.4}
+
+meat = {"Chicken": 2.69,
+        "Beef": 3,
+        "Salami": 4,
+        "Vegan Slice": 3.3}
+
+garnish = {"Onion": 1.69,
+           "Tomato": 1,
+           "Lettuce": 2,
+           "Cheese": 2.5}
+
